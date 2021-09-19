b34fd979ca8a786f39950f39fc214d9e455b378a
TiRajo <88180312+TiRajo@users.noreply.github.com>
2021-09-19 20:04:45 +0530

    Update stream_routes.py

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -17,7 +17,7 @@ async def root_route_handler(request):
     return web.json_response({"status": "running",
                               "maintained_by": "RshkuM",
                               "uptime": get_readable_time(time.time() - StartTime),
-                              "telegram_bot": "Nice Bot")
+                              "telegram_bot": '@'+(await StreamBot.get_me()).username})
 
 
 @routes.get("/{message_id}")
